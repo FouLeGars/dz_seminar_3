364235ef20cb8ca387589dfe4a4d98e496b02b87
Karen Martirosyan <m.k.a@list.ru>
2023-12-07 09:25:26 +0300

    добавил решение задачи 3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+static double[] FillArrayTwo(int length)
+{
+    double[] array = new double[length];
+    Random random = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        double randomNumber = Math.Round(random.NextDouble() * 10, 2);
+        array[i] = randomNumber;
+    }
+    return array;
+}
+
+void PrintArray(double[] MassOn)
+{
+    Console.Write("Массив с элементами: ");
+    for (int i = 0; i < MassOn.Length; i++)
+    {
+        Console.Write(MassOn[i] + " ");
+    }
+}
+
+void DifNnumbers(double[] MassOn)
+{
+    double max = MassOn[0];
+    double min = MassOn[0];
+
+    foreach (double num in MassOn)
+    {
+        if (num > max)
+            max = num;
+
+        if (num < min)
+            min = num;
+    }
+
+    double difference = max - min;
+
+    Console.WriteLine();
+    Console.WriteLine($"Максимальный элемент: {max}");
+    Console.WriteLine($"Минимальный элемент: {min}");
+    Console.WriteLine($"Разница: {difference}");
+}
+
+int arrayLength = 25;
+double[] myArray = FillArrayTwo(arrayLength);
+PrintArray(myArray);
+DifNnumbers(myArray);
